dd9770cb4eed26c46efe263b40ccda88e35f2598
SzAndris1 <andras.szoke@umszki.hu>
2024-06-06 20:07:12 +0200

    Add files via upload

---
+++ argumentumok kód.py
@@ -0,0 +1,12 @@
+def függvény1(keresztnév):
+    print ("Kovács " + keresztnév)
+
+függvény1("László")
+függvény1("József")
+függvény1("Mária")
+
+
+def függvény2(keresztnév, családnév):
+    print(családnév + " " + keresztnév)
+
+függvény2("László", "Kovács")
